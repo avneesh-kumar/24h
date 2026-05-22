423e54fc56d28d847e8a48a38ad220fb7b3d3957
Kunal Chaudhary <kunalchaudhary@rcvtechnologies.com>
2026-05-22 10:37:00 +0530

    remove log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -60,15 +60,6 @@ class AppServiceProvider extends ServiceProvider
             'mail.mailers.smtp.password' => $email->smtpPassword(),
         ]);
 
-        \Log::info('Email settings applied: ', [
-            'from_name' => $email->fromName(),
-            'from_address' => $email->fromAddress(),
-            'smtp_host' => $email->smtpHost(),
-            'smtp_port' => $email->smtpPort(),
-            'smtp_encryption' => $email->smtpEncryption(),
-            'smtp_username' => $email->smtpUsername() ? '***' : null,
-            'smtp_password' => $email->smtpPassword() ? '***' : null,
-        ]);
         // Logging settings
         $logging = app(LoggingSettingsService::class);
         config(['logging.channels.stack.level' => $logging->logLevel()]);
