ac3f0242d06bca371555e1094da50bc38a853347
Kunal Chaudhary <kunalchaudhary@rcvtechnologies.com>
2026-05-27 16:08:44 +0530

    worked on edit blade

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -59,7 +59,7 @@ class ServiceController extends Controller
             'slug' => 'required|string|max:255|unique:services,slug,' . $service->id,
             'thumbnail' => 'nullable|image',
             'banner' => 'nullable|image',
-            'banner_title' => 'nullable|string|max:255',
+            'banner_title' => 'nullable|string|max:500',
             'description' => 'nullable|string',
             'meta_keywords' => 'nullable|string',
             'canonical_url' => 'nullable|url',
